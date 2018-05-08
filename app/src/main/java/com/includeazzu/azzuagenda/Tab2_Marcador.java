c19019dfb1badfe1518b8513a0a768e780ad1dcc
JorgeAzzuFranco <00074216@uca.edu.sv>
2018-05-07 21:45:38 -0600

    pide permiso de añadir contactos (probado) / guardar instancia, favoritos en pestaña

--- app/src/main/java/com/includeazzu/azzuagenda/Tab2_Marcador.java
+++ app/src/main/java/com/includeazzu/azzuagenda/Tab2_Marcador.java
@@ -8,6 +8,7 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
@@ -33,7 +34,7 @@ public class Tab2_Marcador extends Fragment {
     View vMarcador;
 
     EditText nombre;
-    EditText numero2;
+    EditText numero;
     Button guardar;
     ImageButton llamar;
 
@@ -51,7 +52,7 @@ public class Tab2_Marcador extends Fragment {
         vMarcador = inflater.inflate(R.layout.tab2__marcador, container, false);
 
         //Inicializando objetos de la view
-        final EditText numero = vMarcador.findViewById(R.id.editNum);
+        numero = vMarcador.findViewById(R.id.editNum);
         guardar = vMarcador.findViewById(R.id.btnGuardar);
         llamar = vMarcador.findViewById(R.id.btnllamar);
 
@@ -59,7 +60,13 @@ public class Tab2_Marcador extends Fragment {
             @Override
             public void onClick(View view) {
                 //Pide permiso y relaliza
-                pedirPermiso(numero);
+                int permissionCheck = ContextCompat.checkSelfPermission(getContext(), Manifest.permission.CALL_PHONE);
+                if (permissionCheck == PackageManager.PERMISSION_GRANTED){
+                    pedirPermiso(numero);
+                }else {
+                    ActivityCompat.requestPermissions(getActivity(),new String[]{Manifest.permission.READ_CONTACTS},MY_PERMISSIONS_REQUEST_CALL_PHONE);
+                }
+
             }
         });
 
@@ -67,6 +74,27 @@ public class Tab2_Marcador extends Fragment {
     }
 
     final int MY_PERMISSIONS_REQUEST_CALL_PHONE = 0;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        if (requestCode == MY_PERMISSIONS_REQUEST_CALL_PHONE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //Iniciara la llamada
+                strNumero = numero.getText().toString();
+                if(!TextUtils.isEmpty(strNumero)) {
+                    String dial = "tel:" + strNumero;
+                    startActivity(new Intent(Intent.ACTION_CALL, Uri.parse(dial)));
+                }else {
+                    Toast.makeText(getContext(), "Enter a phone number", Toast.LENGTH_SHORT).show();
+                }
+                Toast.makeText(getContext(), "Read Contacts permission granted", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getContext(), "Read Contacts permission denied", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
 
     public void pedirPermiso(EditText numero){
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
